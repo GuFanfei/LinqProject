5fec8471fa29bb32150fec3d81174a56f672fe8c
顾范飞 <gff14810@ly.com>
2017-05-15 10:57:46 +0800

    11

---
+++ LinqProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("1");
+            Console.ReadKey();
+        }
+    }
+}
